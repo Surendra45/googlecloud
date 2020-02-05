2caa748fe6891619ec597043a31bb07ff8ccf52a
Surendra45 <54744237+Surendra45@users.noreply.github.com>
2020-02-05 12:15:36 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
-    class Hello{
-    public static void main(String args[]){
-    System. out. println("Welcome Surendra");
-    }
-    }
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class DemoServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Welcome Surendra");
+  }
+}
